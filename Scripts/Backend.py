049a3ec323a1c01790079269d1f99fc3ce7ed424
Aguniec <aguniecc@gmail.com>
2019-05-19 16:15:41 +0200

    com

--- Scripts/Backend.py
+++ Scripts/Backend.py
@@ -12,7 +12,7 @@ with open("Liczba_osób_które_przystapiły_lub_zdały_egzamin_maturalny.csv", n
         total = []
         for row in csvreader:
             if row["Rok"] == "2010" and row["Terytorium"] == "Lubuskie":
-                total_array = total.append(row["Liczba os?b"])
+                total_array = total.append(row["Liczba osób"])
 
         total_sum = sum([int(i) for i in total])
         total_avg = int(total_sum / len(total))
@@ -24,20 +24,22 @@ with open("Liczba_osób_które_przystapiły_lub_zdały_egzamin_maturalny.csv", n
     def pass_rate():
         proceed_to_the_exam = []
         pass_the_exam = []
+        dict = {}
         for row in csvreader:
-            if row["Przyst?pi?o/zda?o "] == "przyst?pi?o":
-                if row["Rok"] == "2010":
-                    proceed_exam = proceed_to_the_exam.append(row["Liczba os?b"])
+            if row["Rok"] == "2010": 
+                if row["Przystąpiło/zdało"] == "przystąpiło":
+                    proceed_exam = proceed_to_the_exam.append(row["Liczba osób"])
 
-            if row["Przyst?pi?o/zda?o "] == "zda?o":
-                if row["Rok"] == "2010":
-                    pass_exam = pass_the_exam.append(row["Liczba os?b"])
+                if row["Przystąpiło/zdało"] == "zdało":
+                    pass_exam = pass_the_exam.append(row["Liczba osób"])
 
-        proceed_sum = sum([int(i) for i in proceed_to_the_exam])
-        pass_sum = sum([int(i) for i in pass_the_exam])
+            proceed_sum = sum([int(i) for i in proceed_to_the_exam])
+            pass_sum = sum([int(i) for i in pass_the_exam])
 
-        percentage_rate = math.floor(100* (pass_sum / proceed_sum))
+            percentage_rate = math.floor(100* (pass_sum / proceed_sum))
+            dict["2010"] = (str(percentage_rate)+ "%")
         print (percentage_rate, "%")
+        print (dict)
 
     pass_rate()
 
