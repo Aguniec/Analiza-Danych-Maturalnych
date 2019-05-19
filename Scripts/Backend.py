a7b27517bd6ef69a2cf4bed487e376c5c78b6226
Aguniec <aguniecc@gmail.com>
2019-05-19 15:27:08 +0200

    Update Backend.py

--- Scripts/Backend.py
+++ Scripts/Backend.py
@@ -26,9 +26,12 @@ with open("Liczba_osób_które_przystapiły_lub_zdały_egzamin_maturalny.csv", n
         pass_the_exam = []
         for row in csvreader:
             if row["Przyst?pi?o/zda?o "] == "przyst?pi?o":
-                proceed_exam = proceed_to_the_exam.append(row["Liczba os?b"])
-            else:
-                pass_exam = pass_the_exam.append(row["Liczba os?b"])
+                if row["Rok"] == "2010":
+                    proceed_exam = proceed_to_the_exam.append(row["Liczba os?b"])
+
+            if row["Przyst?pi?o/zda?o "] == "zda?o":
+                if row["Rok"] == "2010":
+                    pass_exam = pass_the_exam.append(row["Liczba os?b"])
 
         proceed_sum = sum([int(i) for i in proceed_to_the_exam])
         pass_sum = sum([int(i) for i in pass_the_exam])
