8be24e9d798f876ba6e40fd65e8a0207da04d369
Aguniec <aguniecc@gmail.com>
2019-05-19 14:32:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Recrutiment-Project--Profil-Software
---
+++ Scripts/Backend.py
@@ -0,0 +1,50 @@
+
+
+import csv
+import os
+import math
+#Suma i średnia wszystkich zdających dla danego województwa i roku
+
+with open("Liczba_osób_które_przystapiły_lub_zdały_egzamin_maturalny.csv", newline = "", encoding = "utf-8") as csvfile:
+    csvreader = csv.DictReader(csvfile, delimiter=";")
+
+    def avearge():
+        total = []
+        for row in csvreader:
+            if row["Rok"] == "2010" and row["Terytorium"] == "Lubuskie":
+                total_array = total.append(row["Liczba os?b"])
+
+        total_sum = sum([int(i) for i in total])
+        total_avg = int(total_sum / len(total))
+
+        print("Total avearge: ", total_avg)
+
+#    avearge()
+
+    def pass_rate():
+        proceed_to_the_exam = []
+        pass_the_exam = []
+        for row in csvreader:
+            if row["Przyst?pi?o/zda?o "] == "przyst?pi?o":
+                proceed_exam = proceed_to_the_exam.append(row["Liczba os?b"])
+            else:
+                pass_exam = pass_the_exam.append(row["Liczba os?b"])
+
+        proceed_sum = sum([int(i) for i in proceed_to_the_exam])
+        pass_sum = sum([int(i) for i in pass_the_exam])
+
+        percentage_rate = math.floor(100* (pass_sum / proceed_sum))
+        print (percentage_rate, "%")
+
+    pass_rate()
+
+
+
+
+"""
+import requests
+import json
+
+res = requests.get("https://api.dane.gov.pl/resources/17201")
+res.json()
+"""
